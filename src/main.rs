3bf5707fd86164a64a863d57b8bd9cacec818e8b
Clément Renault <renault.cle@gmail.com>
2019-06-30 12:28:11 +0200

    feat: Ask the compiler to ignore overflows and wrap around memory bounds

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::path::Path;
 
 fn generate_code(input: BufReader<File>, mut output: BufWriter<File>) -> io::Result<()> {
     writeln!(&mut output, "use std::io::{{Read, Write}};")?;
+    writeln!(&mut output, "const MEMORY_LEN: usize = 30_000;")?;
     writeln!(&mut output, "fn main() {{")?;
-    writeln!(&mut output, "let mut memory = [0u8; 30_000];")?;
+    writeln!(&mut output, "let mut memory = [0u8; MEMORY_LEN];")?;
     writeln!(&mut output, "let mut ptr = 0;")?;
     writeln!(&mut output, "let mut stdout = std::io::stdout();")?;
     writeln!(&mut output, "let mut stdin = std::io::stdin();")?;
@@ -14,10 +15,10 @@ fn generate_code(input: BufReader<File>, mut output: BufWriter<File>) -> io::Res
     for c in input.bytes() {
         let c = c?;
         match c {
-            b'>' => writeln!(&mut output, "ptr += 1;")?,
-            b'<' => writeln!(&mut output, "ptr -= 1;")?,
-            b'+' => writeln!(&mut output, "memory[ptr] += 1;")?,
-            b'-' => writeln!(&mut output, "memory[ptr] -= 1;")?,
+            b'>' => writeln!(&mut output, "ptr = (ptr + 1) % MEMORY_LEN;")?,
+            b'<' => writeln!(&mut output, "ptr = ptr.wrapping_sub(1) % MEMORY_LEN;")?,
+            b'+' => writeln!(&mut output, "memory[ptr] = memory[ptr].wrapping_add(1);")?,
+            b'-' => writeln!(&mut output, "memory[ptr] = memory[ptr].wrapping_sub(1);")?,
             b'.' => writeln!(&mut output, "stdout.write_all(&[memory[ptr]]).unwrap();")?,
             b',' => writeln!(&mut output, "stdin.read(&mut memory[ptr..ptr + 1]).unwrap();")?,
             b'[' => writeln!(&mut output, "while memory[ptr] != 0 {{")?,
